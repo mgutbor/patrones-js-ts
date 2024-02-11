9bdc02bf858d6f30b8ffcf1d25d96e24ccfffe13
mgutbor <mgutbor@gmail.com>
2024-02-11 16:26:00 +0100

    01.04 - Arrow funcions

--- 01-intro/js/intro.js
+++ 01-intro/js/intro.js
@@ -1,1 +1,27 @@
-console.log('Hola mundo');
+
+function sum(a,b) {
+  return a+b;
+}
+
+let res = sum (1,2);
+console.log(res);
+
+// Funcion primer orden
+const fSum = sum;
+res = fSum(4,3);
+console.log(res);
+
+// Funcion orden superior
+function operation(fn, a, b) {
+  console.log("se hace algo");
+  console.log(fn(a,b));
+}
+
+res = operation(fSum, 14, 7);
+
+// Arrow function
+let fA = () => console.log('Arrow function');
+
+fA();
+
+operation ((aa,bb)=>aa*bb, 5, 5)
